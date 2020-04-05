d3c318549a92c661c9611face262653104653d71
anton <witold.jnc@gmail.com>
2020-04-05 11:38:53 +0500

    test deploy

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const path = require("path");
-const express = require("express");
-const app = express();
-app.use(express.static(__dirname + '/angular-build'));
-app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname, 'angular-build', 'index.html'))
+const express = require('express');
+const path = require('path');
+const ngApp = express();
+ngApp.use(express.static('./dist/angular-forms-validation'));
+ngApp.get('/*', function (request, response) {
+    response.sendFile(path.join(__dirname, '/dist/angular-forms-validation/index.html'));
 });
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+ngApp.listen(process.env.PORT || 8080);
